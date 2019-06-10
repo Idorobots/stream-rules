74f0cfe7f44fe9ee44c6a4e96eee991a879e92de
Kajetan Rzepecki <k@spartan.works>
2019-06-10 21:53:52 +0200

    Made the streams implementation mutation-free.

--- src/streams.scm
+++ src/streams.scm
@@ -1,38 +1,47 @@
 ;; Reactive streams
 
-(define-struct stream-stage (next emitter) #:mutable)
+(define-struct stream-stage (prev materializer))
+(define-struct materialized-stream-stage (emitter))
 
-(define (source)
-  (make-stream-stage '() push))
+(define ((materialize f) next)
+  (make-materialized-stream-stage (f next)))
+
+(define (stage materializer)
+  (make-stream-stage '() materializer))
 
 (define (push stage value)
-  ((stream-stage-emitter stage) (stream-stage-next stage) value))
+  ((materialized-stream-stage-emitter stage) value))
+
+(define (source)
+  (stage (materialize (lambda (next) (curry push next)))))
 
 (define (flow f)
-  (make-stream-stage '()
-                     (lambda (next value)
-                       (push next (f value)))))
-
-(define (via source flow)
-  (when (null? (stream-stage-next source))
-    (set-stream-stage-next! source flow)
-    flow))
+  (stage (materialize (lambda (next) (compose (curry push next) f)))))
 
 (define (sink f)
-  (make-stream-stage '()
-                     (lambda (_ value)
-                       (f value))))
+  (stage (materialize (lambda (_) f))))
+
+(define (via source flow)
+  (make-stream-stage source
+                     (stream-stage-materializer flow)))
 
 (define to via)
 
-(define (run! stream)
-  stream)
+(define (run sink)
+  (when (stream-stage? sink)
+    (define (run-acc stage acc)
+      (if (null? stage)
+          acc
+          (run-acc (stream-stage-prev stage)
+                   ((stream-stage-materializer stage) acc))))
+    (run-acc sink '())))
 
 (define (map-stream source f)
   (via source (flow f)))
 
 (define (filter-stream source p)
-  (via source (make-stream-stage '()
-                                 (lambda (next value)
-                                   (when (p value)
-                                     (push next value))))))
+  (via source
+       (stage (materialize (lambda (next)
+                             (lambda (value)
+                               (when (p value)
+                                 (push next value))))))))
