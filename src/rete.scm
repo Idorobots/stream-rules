f8cb0309e4e3243ea6831a2147f229c24eab35cc
Kajetan Rzepecki <k@spartan.works>
2019-06-14 23:28:07 +0200

    Cleaned up the Rete implementation.

--- src/rete.scm
+++ src/rete.scm
@@ -10,26 +10,20 @@
         (right (ring-buffer size))
         (combine (lambda (left right next value)
                    (ring-buffer-store! left value)
-                   (ring-buffer-foreach right (lambda (_ v) (f next value v))))))
+                   (ring-buffer-foreach right (lambda (_ v)
+                                                (push next (f value v)))))))
     (combine-streams (-> as (via (stage (curry combine left right))))
                      (-> bs (via (stage (curry combine right left)))))))
 
 (define (also as bs)
-  ((zip-with-memory 100)
-   as
-   bs
-   (lambda (next left right)
-     (let ((r (unify left right)))
-       (unless (null? r)
-         (push next r))))))
+  (-> ((zip-with-memory 100) as bs unify)
+      (filter-stream (compose not null?))))
 
 (define either combine-streams)
 
 (define (matches? stream pattern)
   (-> stream
-      (via (stage (lambda (next value)
-                    (let ((r (pattern-match pattern value)))
-                      (unless (null? r)
-                        (push next r))))))))
+      (map-stream (curry pattern-match pattern))
+      (filter-stream (compose not null?))))
 
 (define whenever map-stream)
